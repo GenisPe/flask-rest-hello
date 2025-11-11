f61b86ed63c72696190876b4b51498dec62b90bb
Genis Pedrós <onlinegenis@gmail.com>
2025-11-11 13:33:29 +0000

    Describe tus cambios

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from sqlalchemy import String, Boolean, ForeignKey, Text, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 db = SQLAlchemy()
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
